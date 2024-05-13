09577eb0aad8414fb10316c58de18c60e6224964
Anindam-Pratap <87144602+Anindam-Pratap@users.noreply.github.com>
2024-05-13 10:54:36 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:5100/api/loginuser', {
+    const response = await fetch('https://gofoooood.netlify.app/loginuser', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
