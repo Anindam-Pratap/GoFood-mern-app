bac7735774e754bfa57b1bede328e28295cb5d91
Anindam-Pratap <87144602+Anindam-Pratap@users.noreply.github.com>
2024-05-13 10:48:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 443
 const connectToMongo = require("./db")
 const https = require('https');
-const fs = require('fs');
+c
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","https://gofoooood.netlify.app")
@@ -24,11 +24,7 @@ connectToMongo()
     app.use('/api', require('./Routes/DisplayData'))
     app.use('/api', require('./Routes/OrderData'))
 
-     // Start the HTTPS server
-  const server = https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app);
+   
     
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
